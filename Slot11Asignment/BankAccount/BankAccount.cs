1e0318cb0684c12f75e1d6ba1a3823d6a15baa07
giang <vuthithaogiang050400@gmail.com>
2023-08-17 15:28:21 +0700

    Add project files.

---
+++ Slot11Asignment/BankAccount/BankAccount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slot11Asignment.BankAccount
+{
+    public delegate void BalanceChangedHandler(decimal newBalance);
+    public class BankAccount
+    {
+        private decimal balance;
+
+        public event BalanceChangedHandler BalanceChanged;
+
+        public BankAccount()
+        {
+            BalanceChanged += SendSMSUpdateBallance;
+        }
+
+        public decimal Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public bool Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                BalanceChanged(amount);
+                SendSMSUpdateBallance(amount);
+                return true;
+            }
+            return false;
+        }
+
+        public bool WithDraw(decimal amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                BalanceChanged(amount);
+                SendSMSUpdateBallance(amount);
+
+
+                return true;
+            }
+            return false;
+        }
+
+        public void SendSMSUpdateBallance(decimal amount)
+        {
+            // send SMS
+        }
+    }
+}
---
+++ Slot11Asignment/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Slot11Asignment.BankAccount;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount();
+        account.Balance = 200;
+
+        //account.BalanceChanged += AccountBalanceChanged;
+
+        account.Deposit(500);
+        Console.WriteLine($"Balance after update: {account.Balance}");
+
+        account.WithDraw(200);
+        Console.WriteLine($"Balance after update: {account.Balance}");
+
+    }
+}
